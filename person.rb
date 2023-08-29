6d6fe214e175cbd757382d8b678425fa3b6d1d97
dani <danifromecuador@gmail.com>
2023-08-29 11:31:07 -0500

    Change parameter name to one shorter

--- person.rb
+++ person.rb
@@ -1,17 +1,17 @@
 class Person
   # This class will not work unless change : for = in parent_permission, but doing that a linter error will appear
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', permission: true)
     @id = rand(1000)
     @name = name
     @age = age
-    @parent_permission = parent_permission
+    @permission = permission
   end
 
   attr_reader :id
   attr_accessor :name, :age
 
   def can_use_services?
-    of_age? || @parent_permission
+    of_age? || @permission
   end
 
   private
