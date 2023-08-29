79e7277ff66e5cf196b2e6e182e0b189881012ee
dani <danifromecuador@gmail.com>
2023-08-29 10:05:27 -0500

    Add argument parent_permission set to true to Teacher sub class

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,8 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(age, specialization, name = 'Unknown')
-    super(age, name)
+  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
     @specialization = specialization
   end
 
