0732e52516b8a4144cf040728c01a44bcb3ad301
dani <danifromecuador@gmail.com>
2023-08-29 20:29:49 -0500

    Create a capitalization method for a given person name

---
+++ capitalize_decorator.rb
@@ -0,0 +1,14 @@
+require_relative 'decorator'
+
+class CapitalizeDecorator
+  def initialize(person)
+    @decorator = Decorator.new
+    @person = person
+  end
+  attr_accessor :person
+  def correct_name
+    person.correct_name.capitalize
+  end
+end
+
+
