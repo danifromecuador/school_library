70d910d1f251c714337faa6d796d882872ccd60f
dani <danifromecuador@gmail.com>
2023-08-31 16:27:02 -0500

    Delete test code

--- person.rb
+++ person.rb
@@ -28,12 +28,3 @@ class Person
     age >= 18
   end
 end
-
-person = Person.new(22, 'maximilianus')
-p person.correct_name
-
-capitalized_person = CapitalizeDecorator.new(person)
-p capitalized_person.correct_name
-
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-p capitalized_trimmed_person.correct_name
