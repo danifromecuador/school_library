dc4beb86fc283e40d658af30c3258a4d80f6aac8
dani <danifromecuador@gmail.com>
2023-08-31 13:43:59 -0500

    Create relationship between classrooms and students

--- classroom.rb
+++ classroom.rb
@@ -1,6 +1,15 @@
 class Classroom
-  def initialize
-    @label
-  end
   attr_accessor :label
+  attr_reader :students
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    students.push(student)
+    student.classroom = self
+  end
+  
 end
--- student.rb
+++ student.rb
@@ -1,14 +1,31 @@
 require_relative 'person'
+require_relative 'classroom'
 
 class Student < Person
-  def initialize(age, classroom = 'unknown', name = 'Unknown', permission: true)
+  attr_accessor :classroom
+
+  def initialize(age, name, classroom = 'unknown', permission: true)
     super(age, name, permission: permission)
     @classroom = classroom
   end
 
-  attr_accessor :classroom
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.push(self) unless classroom.students.include?(self)
+  end
 
   def play_hooky
     '¯(ツ)/¯'
   end
 end
+
+student1 = Student.new(10, 'John')
+student2 = Student.new(12, 'Jane')
+class1 = Classroom.new('B')
+class2 = Classroom.new('A')
+
+class1.add_student(student1)
+class1.add_student(student2)
+
+p student1.classroom.label # => "B"
+p student2.classroom.label # => "B"
