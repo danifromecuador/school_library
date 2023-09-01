b6476a826b2c4f5bf442d62c141403f2f6d068cd
dani <danifromecuador@gmail.com>
2023-08-31 22:38:51 -0500

    Create relationships between Person and Book classes using Rental class

--- person.rb
+++ person.rb
@@ -1,18 +1,21 @@
 require_relative 'nameable'
 require_relative 'capitalize_decorator'
 require_relative 'trimmer_decorator'
+require_relative 'rental'
+require_relative 'book'
 
 class Person
+  attr_reader :id, :rentals
+  attr_accessor :name, :age
+
   def initialize(age, name = 'Unknown', permission: true)
     @id = rand(1000)
     @name = name
     @age = age
     @permission = permission
     @nameable = Nameable.new
+    @rentals = []
   end
-
-  attr_reader :id
-  attr_accessor :name, :age
 
   def can_use_services?
     of_age? || @permission
@@ -20,6 +23,11 @@ class Person
 
   def correct_name
     name
+  end
+
+  def add_rental(book, date)
+    rental = Rental.new(date, self, book)
+    @rentals.push(rental) unless @rentals.include?(rental)
   end
 
   private
--- rental.rb
+++ rental.rb
@@ -1,6 +1,17 @@
 class Rental
   attr_accessor :date
-  def initialize(date)
+  attr_reader :book, :person
+
+  def initialize(date, person, book)
     @date = date
+    @person = person
+    person.add_rental(self)
+    @book = book
+    book.add_rental(self)
   end
 end
+
+book1 = Book.new('The Hobbit', 'J.R.R. Tolkien')
+p book1
+
+Rental.new('2019-01-01')
